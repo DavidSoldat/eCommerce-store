4fb45492e6576c6d660990e9bf661542d67497a4
David Soldat <davidsoldat00@gmail.com>
2025-02-25 20:25:14 +0100

    d

--- eCom-frontend/src/pages/Profilepage.tsx
+++ eCom-frontend/src/pages/Profilepage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { RootState } from "../redux/store";
+import { Divider } from "@mui/material";
 
 export default function Profilepage() {
   const user = useSelector((state: RootState) => state.user.user);
@@ -16,6 +17,15 @@ export default function Profilepage() {
   }, [navigate, role, user]);
 
   return (
-    <div className="mx-auto max-w-[1240px] px-4">{role ? "admin" : "user"}</div>
+    <div className="mx-auto max-w-[1240px] px-4">
+      <Divider />
+      <div className="my-10 flex flex-col gap-5">
+        <h2 className="text-2xl font-semibold">User Profile</h2>
+        <div>
+          <h4>User: {user?.username}</h4>
+          <p>Email: {user?.email}</p>
+        </div>
+      </div>
+    </div>
   );
 }
